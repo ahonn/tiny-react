0c4114ca69103b82d5a8db7d22290dfe70bf8440
ahonn <ahonn95@outlook.com>
2017-06-05 10:45:12 +0800

    change rollup build condig

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,25 @@ var babel =  require('rollup-plugin-babel')
 
 export default {
   entry: 'src/index.js',
-  format: 'umd',
-  moduleName: 'React',
   plugins: [
     babel({
       sourceMap: true,
       exclude: 'node_modules/**'
     })
   ],
-  dest: 'dist/tiny-react.js',
+  targets: [
+    {
+      dest: 'dist/tiny-react.common.js',
+      format: 'cjs'
+    },
+    {
+      dest: 'dist/tiny-react.js',
+      format: 'umd',
+      moduleName: 'TintReact'
+    },
+    {
+      dest: 'example/simple/src/tiny-react.js',
+      format: 'cjs'
+    }
+  ]
 }
